396ffcc1427e2aa77dc1645f3e356e4202fc30eb
David <david2009mn@gmail.com>
2024-02-04 18:35:28 +0200

    Added Flake8 rules

--- project/models.py
+++ project/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Subject(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
         return self.name
+
 
 class Teacher(models.Model):
     name = models.CharField(max_length=100)
@@ -14,12 +16,14 @@ class Teacher(models.Model):
     def __str__(self):
         return self.name
 
+
 class Class(models.Model):
     name = models.CharField(max_length=100)
     teacher = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.name
+
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
@@ -29,6 +33,3 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
-
-
-
