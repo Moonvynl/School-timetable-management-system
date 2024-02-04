498ed317eac53e7efeb8fee5b940e17e77a22773
David <david2009mn@gmail.com>
2024-02-04 18:32:22 +0200

    Added student class

--- project/models.py
+++ project/models.py
@@ -21,5 +21,14 @@ class Class(models.Model):
     def __str__(self):
         return self.name
 
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    address = models.CharField(max_length=50)
+    classes = models.ForeignKey(Class, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
+
 
 
